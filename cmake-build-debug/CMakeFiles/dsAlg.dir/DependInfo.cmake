9e1cbfe2c264cd85bbc327bfe4ede35712f087f5
jumbuna <jacobwangoni@yahoo.com>
2020-08-06 01:13:31 +0300

    imported project into clion for better management using Cmake :)

---
+++ cmake-build-debug/CMakeFiles/dsAlg.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/Pro/Developer/dsAlg/test/substring.cpp" "/Users/Pro/Developer/dsAlg/cmake-build-debug/CMakeFiles/dsAlg.dir/test/substring.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  "../algs"
+  "../algs/searching"
+  "../algs/sorting"
+  "../algs/substring-search"
+  "../documentation"
+  "../ds"
+  "../src"
+  "../test"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
