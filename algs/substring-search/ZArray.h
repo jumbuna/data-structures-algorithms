c8eaed6ae0f3edef5c5cfc9c687976c91b952a25
jumbuna <jacobwangoni@yahoo.com>
2020-08-07 11:16:39 +0300

    removed extra linear complexity in ::zAlgorithmSearch(prev ::ZAlgorithm) by adding valid indicies to return Vector during construction of z-array instead of the linear search on the z-array & also construction of the z-array is now being done inside the ::zAlgorithmSearch function instead of on a separate function (prev ::Zconstruction) :)

--- algs/substring-search/ZArray.h
+++ algs/substring-search/ZArray.h
@@ -19,37 +19,8 @@ void lazyZconstruction(std::string &str, Zarray &array) {
 		array.push_back(l);
 	}
 }
-//O(m+n)
-void zConstruction(std::string &str, Zarray &array) {
-	array.clear();
-	array.push_back(0);
-	int l = 0;
-	for(int i = 1; i < str.length(); i++) {
-		int j = l + i;
-		while(str[j] == str[l] && j < str.length()) {
-			++l;
-			++j;
-		}
-		array.push_back(l);
-		if(l <= 1) {
-			l = 0;
-		}else {
-			int k = 1;
-			for(; k < l && ((array[k] + i+k) < j); k++) {
-				array.push_back(array[k]);
-				++i;
-			}
-			if(i == j) {
-				l = 0;
-				--i;
-			}else {
-				l = array[k];
-			}
-		}
-	}
-}
 
-Vector<int> ZAlgorithm(std::string &string, std::string &pattern, char separator = '$') {
+Vector<int> zAlgorithmSearch(std::string &string, std::string &pattern, char separator = '$') {
 	Vector<int> vec;
 	int patternLength = pattern.length();
 	std::string concated;
@@ -57,16 +28,40 @@ Vector<int> ZAlgorithm(std::string &string, std::string &pattern, char separator
 	concated.push_back(separator);
 	concated.append(string);
 	Zarray array;
-	zConstruction(concated, array);
-	for(int i = 0; i < array.size(); i++) {
-		if(array[i] == patternLength) {
-			vec.push_back(i-patternLength-1);
+	//(m+n) time z-array construction
+	array.push_back(0);
+	int previousMatches = 0;
+	for(int i = 1; i < concated.length(); i++) {
+		int mismatchIndex = previousMatches + i;
+		while(concated[mismatchIndex] == concated[previousMatches] && mismatchIndex < concated.length()) {
+			++previousMatches;
+			++mismatchIndex;
+		}
+		array.push_back(previousMatches);
+		if(previousMatches == patternLength) {
+			vec.push_back(array.size() - patternLength - 2);
+		}
+		if(previousMatches <= 1) {
+			previousMatches = 0;
+		}else {
+			int k = 1;
+			for(; k < previousMatches && ((array[k] + i+k) < mismatchIndex); k++) {
+				array.push_back(array[k]);
+				++i;
+			}
+			if(i == mismatchIndex) {
+				previousMatches = 0;
+				--i;
+			}else {
+				previousMatches = array[k];
+			}
 		}
 	}
+	
 	return vec;
 }
 
-Vector<int> ZAlgorithm(std::string &&string, std::string &&pattern, char separator = '$') {
-	return ZAlgorithm(string, pattern, separator);
+Vector<int> zAlgorithmSearch(std::string &&string, std::string &&pattern, char separator = '$') {
+	return zAlgorithmSearch(string, pattern, separator);
 }
 }
--- test/substring.cpp
+++ test/substring.cpp
@@ -16,7 +16,7 @@ int main(int argc, char *argv[]) {
 	std::cout << jumbuna::bruteSearch("abcxabcdabxabcdabcdabcy", "abcdabcy") << std::endl;
 	std::cout << jumbuna::rabinKarp("abcxabcdabxabcdabcdabcy", "abcdabcy") << std::endl;
 	std::cout << jumbuna::bayerMooreSearch("abcxabcdabxabcdabcdabcy", "abcdabcy") << std::endl;
-	auto vec = jumbuna::ZAlgorithm("abcxabcdabxabcdabcdabcy", "abcdabcy");
+	auto vec = jumbuna::zAlgorithmSearch("abcxabcdabxabcdabcdabcy", "abcdabcy");
 	for(int i = 0; i < vec.size(); i++) {
 		std::cout << vec[i] << std::endl;
 	}
