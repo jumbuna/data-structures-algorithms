e6f9ecca13cc77ddf11fc645fc9e362b80862674
jumbuna <jacobwangoni@yahoo.com>
2020-08-08 17:25:13 +0300

    while implementing binary trees in java i figured out a more cleaner and concise way of implementing the remove algorithm i.e (reduced Rb code by roughly 50 lines) :)

--- ds/RedBlackTreeImpl.h
+++ ds/RedBlackTreeImpl.h
@@ -35,30 +35,30 @@ void RedBlackTree<T, C>::insert(Node *candidate, Node *parent, T element) {
 	if(candidate == &leafSentinel && parent == &leafSentinel) {
 		candidate = nodeAllocator.create(element, nullptr, &leafSentinel); 
 		BinarySearchTree<T, C>::root = candidate;
-	}else if(candidate == &leafSentinel) {
+		return;
+	}
+	if(candidate == &leafSentinel) {
 		candidate = nodeAllocator.create(element, parent, &leafSentinel);
 		if(BinarySearchTree<T, C>::comparator(parent->element, element)) {
 			parent->leftChild = candidate;
 		}else {
 			parent->rightChild = candidate;
 		}
-	}else {
-		if(BinarySearchTree<T, C>::comparator(candidate->element, element)) {
-			insert(candidate->leftChild, candidate, element);
-		}else {
-			insert(candidate->rightChild, candidate, element);
-		}
+		balance(static_cast<RbNode*>(candidate));
+		return;
 	}
-		
-	balance(static_cast<RbNode*>(candidate));
+	if(BinarySearchTree<T, C>::comparator(candidate->element, element)) {
+		insert(candidate->leftChild, candidate, element);
+	}else {
+		insert(candidate->rightChild, candidate, element);
+	}
 }
 
 template<class T, class C>
 void RedBlackTree<T, C>::balance(RbNode *candidate) {
-	if(candidate->color == BLACK) {
-		return;
+	if(candidate->color == RED) {
+		insertCaseOne(candidate);
 	}
-	insertCaseOne(candidate);
 }
 
 template<class T, class C>
@@ -91,83 +91,51 @@ void RedBlackTree<T, C>::insertCaseThree(RbNode *candidate, RbNode *parent) {
 			return;
 		}
 	}
-	insertCaseFour(candidate, parent);
+	insertCaseFour(candidate, parent, grandParent, parent == grandParent->leftChild, candidate == parent->leftChild);
 }
 
 template<class T, class C>
-void RedBlackTree<T, C>::insertCaseFour(RbNode *candidate, RbNode *parent) {
-	RbNode *grandParent = parent->getParent();
-	if(grandParent->getLeftChild() == parent) {
-		if(candidate == parent->getLeftChild()) {
-			BstUtility<T, C>::leftLeftCase(parent, this);
-			parent->color = BLACK;
-			grandParent->color = RED;
-		}else {
-			BstUtility<T, C>::leftRightCase(parent, this);
-			candidate->color = BLACK;
-			grandParent->color = RED;
-		}
-	}else {
-		if(candidate == parent->getRightChild()) {
-			BstUtility<T, C>::rightRightCase(parent, this);
-			parent->color = BLACK;
-			grandParent->color = RED;
-		}else {
-			BstUtility<T, C>::rightLeftCase(parent, this);
-			candidate->color = BLACK;
-			grandParent->color = RED;
-		}
+void RedBlackTree<T, C>::insertCaseFour(RbNode *candidate, RbNode *parent, RbNode *grandParent, bool parentIsLeft, bool candidateIsLeft) {
+	grandParent->color = RED;
+	if(parentIsLeft && candidateIsLeft) {
+		BstUtility<T, C>::leftLeftCase(parent, this);
+		parent->color = BLACK;
+	} else if(parentIsLeft) {
+		BstUtility<T, C>::leftRightCase(candidate, this);
+		candidate->color = BLACK;
+	} else if(candidateIsLeft) {
+		BstUtility<T, C>::rightLeftCase(candidate, this);
+		candidate->color = BLACK;
+	} else {
+		BstUtility<T, C>::rightRightCase(parent, this);
 	}
 }
 
 template<class T, class C>
 void RedBlackTree<T, C>::remove(RbNode *candidate, T element) {
-	if(candidate == &leafSentinel) {
-		return;
-	}
+	if(candidate == &leafSentinel) return;
 	if(candidate->element != element) {
-		if(BinarySearchTree<T, C>::comparator(candidate->element, element)) {
-			remove(candidate->getLeftChild(), element);
-		}else {
-			remove(candidate->getRightChild(), element);
-		}
+		if(BinarySearchTree<T, C>::comparator(candidate->element, element)) remove(candidate->getLeftChild(), element);
+		else remove(candidate->getRightChild(), element);
 		return;
 	}
-	
 	RbNode *parent = candidate->getParent();
-	if(candidate->rightChild == &leafSentinel && candidate->leftChild == &leafSentinel) {
+	if(candidate->rightChild == &leafSentinel || candidate->leftChild == &leafSentinel) {
+		RbNode *child = candidate->rightChild == &leafSentinel ? candidate->getLeftChild() : candidate->getRightChild();
 		if(candidate->color == BLACK) {
-			deleteCaseOne(candidate);
+			if(child->color == BLACK) deleteCaseOne(candidate);
+			else child->color = BLACK;
 		}
 		if(parent) {
-			parent->getLeftChild() == candidate ? parent->leftChild = &leafSentinel : parent->rightChild = &leafSentinel;
+			if(parent->leftChild == candidate) parent->leftChild = child;
+			else parent->rightChild = child;
+		} else {
+			BinarySearchTree<T, C>::root = child;
 		}
-		
-	}else if(candidate->rightChild == &leafSentinel) {
-		RbNode *leftChild = candidate->getLeftChild();
-		if(candidate->color == BLACK) {
-			leftChild->color = BLACK;
-		}
-		if(parent) {
-			parent->getLeftChild() == candidate ? parent->leftChild = leftChild : parent->rightChild = leftChild;
-		}else {
-			BinarySearchTree<T, C>::root = leftChild;
-		}
-		leftChild->parent = parent;
+		if(child != &leafSentinel) child->parent = parent;
 		nodeAllocator.destroy(candidate);
-	} else if(candidate->leftChild == &leafSentinel) {
-		RbNode *rightChild = candidate->getRightChild();
-		if(candidate->color == BLACK) {
-			rightChild->color = BLACK;
-		}
-		if(parent) {
-			parent->getLeftChild() == candidate ? parent->leftChild = rightChild : parent->rightChild = rightChild;
-		}else {
-			BinarySearchTree<T, C>::root = rightChild;
-		}
-		rightChild->parent = parent;
-		nodeAllocator.destroy(candidate);
-	}else {
+		--BinarySearchTree<T, C>::nodeCount;
+	} else {
 		T successor = BstUtility<T, C>::preOrderSuccessor(candidate->leftChild, &leafSentinel);
 		candidate->element = successor;
 		remove(candidate->getLeftChild(), successor);
@@ -176,11 +144,8 @@ void RedBlackTree<T, C>::remove(RbNode *candidate, T element) {
 
 template<class T, class C>
 void RedBlackTree<T, C>::deleteCaseOne(RbNode *doubleBlackNode) {
-	if(doubleBlackNode == BinarySearchTree<T, C>::root) {
-		doubleBlackNode->color = BLACK;
-	}else {
-		deleteCaseTwo(doubleBlackNode);
-	}
+	if(doubleBlackNode == BinarySearchTree<T, C>::root) doubleBlackNode->color = BLACK;
+	else deleteCaseTwo(doubleBlackNode);
 }
 
 template<class T, class C>
@@ -188,34 +153,30 @@ void RedBlackTree<T, C>::deleteCaseTwo(RbNode *doubleBlackNode) {
 	RbNode *parent = doubleBlackNode->getParent();
 	RbNode *sibling = parent->getLeftChild() == doubleBlackNode ? parent->getRightChild() : parent->getLeftChild();
 	if(sibling->color == RED) {
-		if(sibling == parent->getLeftChild()) {
-			BstUtility<T, C>::leftLeftCase(sibling, this, &leafSentinel);
-		}else {
-			BstUtility<T, C>::rightRightCase(sibling, this, &leafSentinel);
-		}
+		if(sibling == parent->getLeftChild()) BstUtility<T, C>::leftLeftCase(sibling, this, &leafSentinel);
+		else BstUtility<T, C>::rightRightCase(sibling, this, &leafSentinel);
 		parent->color = RED;
 		sibling->color = BLACK;
+		deleteCaseFour(doubleBlackNode, parent);
+		return;
 	}
-	deleteCaseThree(doubleBlackNode);
+	deleteCaseThree(doubleBlackNode, parent, sibling);
 }
 
 template<class T, class C>
-void RedBlackTree<T, C>::deleteCaseThree(RbNode *doubleBlackNode) {
-	RbNode *parent = doubleBlackNode->getParent();
-	RbNode *sibling = parent->getLeftChild() == doubleBlackNode ? parent->getRightChild() : parent->getLeftChild();
+void RedBlackTree<T, C>::deleteCaseThree(RbNode *doubleBlackNode, RbNode *parent, RbNode *siblng) {
 	RbNode *leftNephew = sibling->getLeftChild();
 	RbNode *rightNephew = sibling->getRightChild();
 	if(parent->color == BLACK && sibling->color == BLACK && leftNephew->color == BLACK && rightNephew->color == BLACK) {
 		sibling->color = RED;
 		deleteCaseOne(parent);
-	}else {
-		deleteCaseFour(doubleBlackNode);
+		return;
 	}
+	deleteCaseFour(doubleBlackNode, parent);
 }
 
 template<class T, class C>
-void RedBlackTree<T, C>::deleteCaseFour(RbNode *doubleBlackNode) {
-	RbNode *parent = doubleBlackNode->getParent();
+void RedBlackTree<T, C>::deleteCaseFour(RbNode *doubleBlackNode, RbNode *parent) {
 	RbNode *sibling = parent->getLeftChild() == doubleBlackNode ? parent->getRightChild() : parent->getLeftChild();
 	RbNode *leftNephew = sibling->getLeftChild();
 	RbNode *rightNephew = sibling->getRightChild();
@@ -223,14 +184,12 @@ void RedBlackTree<T, C>::deleteCaseFour(RbNode *doubleBlackNode) {
 		sibling->color = RED;
 		parent->color = BLACK;
 	}else {
-		deleteCaseFive(doubleBlackNode);
+		deleteCaseFive(doubleBlackNode, parent, sibling);
 	}
 }
 
 template<class T, class C>
-void RedBlackTree<T, C>::deleteCaseFive(RbNode *doubleBlackNode) {
-	RbNode *parent = doubleBlackNode->getParent();
-	RbNode *sibling = parent->getLeftChild() == doubleBlackNode ? parent->getRightChild() : parent->getLeftChild();
+void RedBlackTree<T, C>::deleteCaseFive(RbNode *doubleBlackNode, RbNode *parent, RbNode *sibling) {
 	RbNode *leftNephew = sibling->getLeftChild();
 	RbNode *rightNephew = sibling->getRightChild();
 	if(parent->getLeftChild() == sibling) {
@@ -246,13 +205,11 @@ void RedBlackTree<T, C>::deleteCaseFive(RbNode *doubleBlackNode) {
 			sibling->color = RED;
 		}
 	}
-	deleteCaseSix(doubleBlackNode);
+	deleteCaseSix(doubleBlackNode, parent, sibling);
 }
 
 template<class T, class C>
-void RedBlackTree<T, C>::deleteCaseSix(RbNode *doubleBlackNode) {
-	RbNode *parent = doubleBlackNode->getParent();
-	RbNode *sibling = parent->getLeftChild() == doubleBlackNode ? parent->getRightChild() : parent->getLeftChild();
+void RedBlackTree<T, C>::deleteCaseSix(RbNode *doubleBlackNode, RbNode *parent, RbNode *sibling) {
 	RbNode *leftNephew = sibling->getLeftChild();
 	RbNode *rightNephew = sibling->getRightChild();
 	if(parent->getLeftChild() == sibling) {
@@ -289,12 +246,7 @@ void RedBlackTree<T, C>::insert(T element) {
 
 template<class T, class C>
 void RedBlackTree<T, C>::remove(T element) {
-	if(contains(element)) {
-		remove(static_cast<RbNode*>(BinarySearchTree<T, C>::root), element);
-		if(--BinarySearchTree<T, C>::nodeCount == 0) {
-			BinarySearchTree<T, C>::root = &leafSentinel;
-		}
-	}
+	remove(static_cast<RbNode*>(BinarySearchTree<T, C>::root), element);
 }
 
 template<class T, class C>
@@ -314,9 +266,7 @@ Vector<T> RedBlackTree<T, C>::treeTraversal(TraversalOrder order) {
 
 template<class T, class C>
 void RedBlackTree<T, C>::clear() {
-	if(size() > 0) {
-		nodeAllocator.reset();
-	}
+	if(size() > 0) nodeAllocator.reset();
 	BinarySearchTree<T, C>::root = &leafSentinel;
 	BinarySearchTree<T, C>::nodeCount = 0;
 }
